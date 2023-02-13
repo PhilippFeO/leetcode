5281c88ad1ad79e7ae37855b5fdc37791d3d412e
Philipp Rost <philipp.rost@fau.de>
2023-02-13 19:22:45 +0100

    Now I use WORDcount to count WORDS not lines

--- 194-transpose-file.sh
+++ 194-transpose-file.sh
@@ -13,7 +13,8 @@ cat /dev/null > $transposed_file
 # count ' ' (spaces) in the first line as the are the delimiter
 #   no trailing spaces assumed
 #   +1 because there is always one delimiter less than entries
-nmb_cols=$(($(head -1 194-file.txt | grep -o " " | wc -l) + 1))
+#nmb_cols=$(($(head -1 194-file.txt | grep -o " " | wc -l) + 1))
+nmb_cols=$(head -1 194-file.txt | wc -w)
 # iterate over evey column
 for i in $(seq 1 $nmb_cols); do
     # extract column, replace newlines with spaces, append to output file
